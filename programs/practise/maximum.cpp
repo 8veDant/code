e7d0d61d40d63867e97587739b2f340c41a618ba
8veDant <gaikwadvedant30@gmail.com>
2021-04-12 21:04:48 +0530

    maximum of 2 numbers

---
+++ programs/practise/maximum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+  int a,b;
+  cout<<"Enter the numbers : ";
+  cin>>a>>b;
+  if (a>b)
+  {
+      cout<<a<<" is maximum.";
+  }  
+  else
+  {
+      cout<<b<<" is maximum.\n";
+  }
+return 0;
+}
