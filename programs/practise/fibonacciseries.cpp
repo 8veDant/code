87788f4bbfbdfce7ea2c82c6bdcd0edf6bf325b3
8veDant <gaikwadvedant30@gmail.com>
2021-04-18 19:02:08 +0530

    Fibonacci series

---
+++ programs/practise/fibonacciseries.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    int n,t1=0,t2=1,nextTerm=0;
+    cout<<"Enter number:";
+    cin>>n;
+    cout<<"Fibonacci series:"<<t1<<" , "<<t2<<" , ";
+    nextTerm=t1+t2;
+    while (nextTerm<=n)
+    {
+        cout<<nextTerm<<" , ";
+        t1=t2;
+        t2=nextTerm;
+        nextTerm=t1+t2;
+    }   
+    
+    return 0;
+}
