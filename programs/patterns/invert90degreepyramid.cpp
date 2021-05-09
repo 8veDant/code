42059d1d7627730977bf18578f6bd83836ce1fea
vedant <gaiwkadvedant30@gmail.com>
2021-05-09 15:47:45 +0530

    Symbol choise.

--- programs/patterns/invert90degreepyramid.cpp
+++ programs/patterns/invert90degreepyramid.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 int main() 
 {
     int n;
+    char symbol;
     cout<<"Enter the value: ";
     cin>>n;
+    cout<<"Enter the symbol: ";
+    cin>>symbol;
     for (int i = n; i>=1 ; i--)
     {
         for (int j = 1; j<=i; j++)
         {
-            cout<<"*";
+            cout<<setw(2)<<symbol;
         }
         cout<<"\n";
           
